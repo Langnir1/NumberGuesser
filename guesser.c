ff737a51b9734b02ca7c8200e8f7d7c302b2e5a3
Langnir1 <joshua.halamka@bsu.edu>
2025-01-21 17:01:24 +0000

    Should be working now

--- guesser.c
+++ guesser.c
@@ -1,5 +1,39 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 int main(){
-	printf("hello \n");
+	int turns = 0;
+	int correct;
+	int guess;
+	char name[20];
+	bool keepGoing = true;
+
+	srand(time(NULL));
+
+	printf("Please enter your name: ");
+	scanf("%s", name);
+	printf("Hello %s. A game we shall play. \n", name);
+
+	correct = (rand() %  100) + 1;
+
+	printf("Correct number is: %d", correct);
+
+	while (keepGoing){
+		turns++;
+		printf("(Turn %d) Please enter a guess: ", turns);
+		scanf("%d", &guess);
+		if(guess < correct){
+			printf("Too Low! \n");
+		} else if(guess > correct){
+			printf("Too High! \n");
+		} else{
+			printf("You win! \n");
+			keepGoing = false;
+		}
+
+	} // end while loop
 	return(0);
-}
+
+} // end main
